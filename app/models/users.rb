db49d10b514e4d3bb619274f39e29d224fa6d0b1
AmineHLub <Amino.bhl@gmail.com>
2022-01-18 09:40:53 +0100

    Add user most recent posts method

--- app/models/users.rb
+++ app/models/users.rb
@@ -2,4 +2,7 @@ class Users < ApplicationRecord
     has_many :posts 
     has_many :likes, through: :posts           
     has_many :comments, through: :posts    
+    def last_recent_posts
+    posts.last(3)
+    end
 end
