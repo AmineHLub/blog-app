54dddac8872ed553ec49f814a78527d9b87bc4ed
AmineHLub <Amino.bhl@gmail.com>
2022-01-18 09:43:00 +0100

    Add post methods

--- app/models/posts.rb
+++ app/models/posts.rb
@@ -1,4 +1,13 @@
 class Posts < ApplicationRecord      
-    belongs_to :user
+    belongs_to :users
     has_many :likes, :comments
+
+    def update_post_counter
+    users.posts_counter += 1
+    end
+
+
+    def recent_comments
+    comments.last(5)
+    end
 end
