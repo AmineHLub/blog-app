a2e9dcc8c0a2d6a406c30e361a945bd13e4fdf91
AmineHLub <Amino.bhl@gmail.com>
2022-01-19 16:07:45 +0100

    Finish user-show setup statically with design

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,4 +10,9 @@ class Post < ApplicationRecord
   def recent_comments
     comments.last(5)
   end
+
+  User.create(name: 'Jhon', photo: 'Link to a photo', bio: 'I am a developer', posts_counter: 0)
+  User.create(name: 'Bella', photo: 'Link to a photo', bio: 'I am a teacher', posts_counter: 0)
+  User.create(name: 'Kim', photo: 'Link to a photo', bio: 'I am a nurse', posts_counter: 0)
+  User.create(name: 'Mohamed', photo: 'Link to a photo', bio: 'I am a technician', posts_counter: 0)
 end
