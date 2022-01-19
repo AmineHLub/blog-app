8d0fe43cdff911219438d5b3aaec77b19e28e575
AmineHLub <Amino.bhl@gmail.com>
2022-01-19 18:36:12 +0100

    Add data to user from db dynamically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where('user_id = ?', params[:id])
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -11,8 +11,8 @@ class Post < ApplicationRecord
     comments.last(5)
   end
 
-  User.create(name: 'Jhon', photo: 'Link to a photo', bio: 'I am a developer', posts_counter: 0)
-  User.create(name: 'Bella', photo: 'Link to a photo', bio: 'I am a teacher', posts_counter: 0)
-  User.create(name: 'Kim', photo: 'Link to a photo', bio: 'I am a nurse', posts_counter: 0)
-  User.create(name: 'Mohamed', photo: 'Link to a photo', bio: 'I am a technician', posts_counter: 0)
+  Post.create(title: 'Post of Jhon', likes_counter: 1, comments_counter: 1, user_id: 1, text: 'this is a post by john')
+  Post.create(title: 'Post of Bella', likes_counter: 5, comments_counter: 7, user_id: 2, text: 'this is a post by bella')
+  Post.create(title: 'Post of Kim', likes_counter: 7, comments_counter: 2, user_id: 3, text: 'this is a post by kim')
+  Post.create(title: 'Post of Mohamed', likes_counter: 9, comments_counter: 0, user_id: 4, text: 'this is a post by mohamed')
 end
