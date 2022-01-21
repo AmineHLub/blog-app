16ff0adae446b54cd4f30753a7af8cd50a3fa99e
AmineHLub <Amino.bhl@gmail.com>
2022-01-21 15:32:59 +0100

    Add routing for post form, readjust controller so not all users can acess all posts(only theirs)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,8 @@ class PostsController < ApplicationController
 
   def show
     @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
+    @post = @user.posts.find(params[:id])
   end
+
+  def new; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
+  end  
+
+  resources :posts, only: [:new, :create] do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
   end
   root to: 'users#index'
   # Defines the root path route ("/")
