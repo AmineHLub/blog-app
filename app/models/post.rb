6df6bceb8975e73265e30bb2ba65dfe4c4fb0b2b
AmineHLub <Amino.bhl@gmail.com>
2022-01-22 15:16:51 +0100

    Add validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
   belongs_to :user
   has_many :likes
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0}
-  
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   has_many :posts
   has_many :likes, through: :posts
   has_many :comments, through: :posts
