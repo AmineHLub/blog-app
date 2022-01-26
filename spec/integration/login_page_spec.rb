7092e0b23d768eeaafc11778e396f1f2ec5bd592
AmineHLub <Amino.bhl@gmail.com>
2022-01-26 17:21:47 +0100

    Fix login specs - Delete unecessary creations

--- spec/integration/login_page_spec.rb
+++ spec/integration/login_page_spec.rb
@@ -1,26 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Login page', js: true, type: :feature do
-  before(:all) do
-    @first_user = User.create(name: 'Mark', photo: 'photos_url',
-                              bio: 'Stuff about mark',
-                              posts_counter: 0, email: 'mark@gmail.com',
-                              password: '0123456', confirmed_at: Time.now)
-    @second_user = User.create(name: 'Angela', photo: 'photos_url',
-                               bio: 'Stuff about her',
-                               posts_counter: 0, email: 'angel@gmail.com', password: 'abcdefg', confirmed_at: Time.now)
-    @first_post = Post.create(title: 'My first post', text: 'This is my first post',
-                              user_id: @first_user.id, comments_counter: 0, likes_counter: 0)
-    @second_post = Post.create(title: 'My second post', text: 'This is my second post',
-                               user_id: @first_user.id, comments_counter: 0, likes_counter: 0)
-    @third_post = Post.create(title: 'My third post', text: 'This is my third post',
-                              user_id: @first_user.id, comments_counter: 0, likes_counter: 0)
-    @fourth_post = Post.create(title: 'My fourth post', text: 'This is my fourth post',
-                               user_id: @first_user.id, comments_counter: 0, likes_counter: 0)
-    Comment.create(text: 'This is my first comment', user_id: @second_user.id, post_id: @fourth_post.id)
-    Comment.create(text: 'This is my second comment', user_id: @first_user.id, post_id: @fourth_post.id)
-  end
-
   describe 'Login Page requirements' do
     it 'has username and password inputs, and Log in button' do
       visit new_user_session_path
@@ -48,9 +28,13 @@ RSpec.describe 'Login page', js: true, type: :feature do
     end
 
     it 'can log in with correct data' do
+      @first_user = User.create(name: 'Mark', photo: 'photos_url',
+                                bio: 'Stuff about mark',
+                                posts_counter: 0, email: 'mark@gmail.com',
+                                password: '0123456', confirmed_at: Time.now)
       visit new_user_session_path
-      fill_in 'user_email', with: 'angel@gmail.com'
-      fill_in 'user_password', with: 'abcdefg'
+      fill_in 'user_email', with: @first_user.email
+      fill_in 'user_password', with: @first_user.password
       click_button 'Log in'
       expect(page).to have_content('Logout')
     end
