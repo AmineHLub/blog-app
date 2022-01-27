c98824458a962d0fc120a200e5e3ad24428e6ac3
AmineHLub <Amino.bhl@gmail.com>
2022-01-27 22:12:44 +0100

    Fix: Now api routes response only with Json

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,10 +1,6 @@
 class Api::V1::PostsController < Api::V1::ApiController
   before_action :authenticate_user!
-
-  def show
-    @post = Post.find(params[:id])
-    render json: @post, status: 200
-  end
+  load_and_authorize_resource
 
   def index
     @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    namespace :v1  do 
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do 
       resources :posts, only: [:index] do
         resources :comments, only: [:create, :index]
       end
