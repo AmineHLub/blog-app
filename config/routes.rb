8605cbcfee40761fbbc5a2f536036f3ff9c769cb
see-why <iyadicy@gmail.com>
2022-01-27 18:04:09 +0100

    Add routes for APIs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :update] do
+       resources :posts do
+        resources :comments, only: [:create, :destroy]
+        resources :likes, only: [:create]
+       end
+      end
+    end
+  end
+
   devise_for :users
+
+  default_url_options :host => 'localhost'
 
   devise_scope :user do
     # write all your routes inside this block
