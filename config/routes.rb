62f16cfbd25575551a0f6e5b35a0b8a2e66271af
AmineHLub <Amino.bhl@gmail.com>
2022-01-27 21:59:52 +0100

    fix routes and delete user unecessary api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    namespace :v1 do
-      resources :users, only: [:index, :create, :update] do
-       resources :posts do
-        resources :comments, only: [:create, :destroy]
-        resources :likes, only: [:create]
-       end
+    namespace :v1  do 
+      resources :posts, only: [:index] do
+        resources :comments, only: [:create, :index]
       end
     end
   end
